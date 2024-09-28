5bdf2ed19cf8b4322f83d5304054f934c9c9767c
Waseem <abap.waseem@gmail.com>
2024-09-28 14:36:26 -0700

    commit-6

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const port = process.env.PORT || 3000
+
 //Construct directry location for express config
 const publicDirectory = path.join(__dirname,'../public')
 const viewDirectory = path.join(__dirname,'../templates/views')
@@ -91,6 +93,8 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('Server is up on port' + port)
 })
+
+
